c88e0938032a07b48d2e721b9205657b55d4f328
Ay_HY <Ayccccio@outlook.com>
2022-05-13 17:40:12 +0800

    ajax

---
+++ 04.练习/03.AJAX/js/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const prot = 80
+
+app.all('/',(req, res)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Headers','*')
+    const data = {name:'ay'};
+    res.send(JSON.stringify(data))
+})
+
+app.listen(prot, ()=>{
+    console.log('express监听已启动,端口' + prot)
+})
