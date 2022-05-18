fef01ab81c1113edc63b901dd1d32766f96b2cff
Ay_HY <Ayccccio@outlook.com>
2022-05-18 11:46:51 +0800

    ajax

--- 04.练习/03.AJAX/js/server.js
+++ 04.练习/03.AJAX/js/server.js
@@ -3,12 +3,12 @@ const app = express()
 const prot = 80
 
 app.all('/',(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
-    res.setHeader('Access-Control-Allow-Headers','*')
+    res.setHeader('Access-Control-Allow-Origin','*')    //设置跨域请求
+    res.setHeader('Access-Control-Allow-Headers','*')   //设置允许自定义请求头
     const data = {name:'ay'};
-    res.send(`handle(${JSON.stringify(data)})`)
+    res.send(`handle(${JSON.stringify(data)})`)         //向客户端发送数据
 })
 
-app.listen(prot, ()=>{
+app.listen(prot, ()=>{                                  //设置监听端口
     console.log('express监听已启动,端口' + prot)
 })
