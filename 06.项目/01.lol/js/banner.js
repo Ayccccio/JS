a16b89bebce9981d5cc3ae4d1b2e68195492005c
Ayccccio_home <Ayccccio@gmail.com>
2022-05-30 23:48:23 +0800

    banner

--- 06.项目/01.lol/js/banner.js
+++ 06.项目/01.lol/js/banner.js
@@ -1,10 +1,32 @@
-import { getStyle } from "./tools";
+import { motion, toggleClass } from "./tools.js";
+
+// banner按钮切换
+function toggleBtn(){
+    toggleClass(oldBtn,'sel')
+    toggleClass(elBannerTitles[index],'sel')
+    oldBtn = elBannerTitles[index]
+}
 
 // banner
-let elBannerTitles = document.querySelectorAll('.banner-thile li');
+let elBannerTitles = document.querySelectorAll('.banner-title li');
 let elBanner= document.querySelector('.banner');
 
 
 // banner复制第一个元素插入尾部
+let bannerW = elBanner.parentElement.clientWidth;
 elBanner.appendChild(elBanner.firstElementChild.cloneNode(true));
-elBanner.style.width = elBanner.clientWidth * 
+elBanner.style.width = bannerW * elBanner.children.length + 'px';
+
+// 定时器,自动播放banner
+let index = 0;
+let oldBtn = elBannerTitles[index];
+setInterval(()=>{
+    index++;
+    toggleBtn()
+    motion(elBanner,{'margin-left': parseInt(bannerW) * -index + 'px'},()=>{
+        if(index >= elBanner.children.length - 1){
+            elBanner.style.marginLeft = 0;
+            index=0;
+        }
+    },200,1)
+},2800)
--- 06.项目/01.lol/js/tools.js
+++ 06.项目/01.lol/js/tools.js
@@ -1,3 +1,5 @@
+import {Tween} from './animate.js'
+
 // 函数封装
 /**
  * 获取元素样式
@@ -81,4 +83,62 @@ function toggleClass(el, className){
     }
 }
 
-export{getStyle, addEvent, addClass, removeClass, hasClass, toggleClass}
+/*
+    运动函数
+    el: 元素
+    className: 要添加的类名string类型
+ */
+    function motion(el, styleJson, callBack, time = 1000, timeout = 5, tween = 'Linear'){
+        // 清除Interval,防止重复执行
+        clearInterval(el.timer);
+        
+        var startJ = {},    //元素样式开始值
+            endJ = {},      //元素样式结束值
+            changeJ = {},   //元素样式变化值
+            count = time / timeout,     //变化次数
+            index = 0;      //当前次数
+            
+        // 获取元素开始，结束，变化值
+        for (const key in styleJson) {
+            startJ[key] = parseFloat(getStyle(el, key));
+            endJ[key] = parseFloat(styleJson[key]);
+            changeJ[key] = endJ[key] - startJ[key];
+        }
+    
+        // 定时器开始元素变化
+        el.timer = setInterval(function(){
+            index++;
+            // 拉终停表
+            if(index >= count){
+                // 将样式值设置为最终值，避免因步长原因导致最后值差一点
+                for (const key in endJ) {
+                    // 判断是否是opacity
+                    if(key == 'opacity'){
+                        el.style[key] = endJ[key];
+                        e.style.filter = 'Alpha(opacity=' + endJ[key] * 100 + ')';
+                    }
+                    el.style[key] = endJ[key] + 'px';
+                }
+    
+                // 运动结束执行回调
+                callBack && callBack.call(el);
+    
+                clearInterval(el.timer);
+                return;
+            }
+    
+            // 开始运动
+            for (const key in endJ) {
+                // 运动函数计算每步移动距离
+                var step = Tween[tween](index, startJ[key], changeJ[key], count);
+                // 判断是否是opacity
+                if(key == 'opacity'){
+                    el.style[key] = step;
+                    e.style.filter = 'Alpha(opacity=' + step * 100 + ')';
+                }
+                el.style[key] = step + 'px';
+            }
+        },timeout)
+    }
+
+export{getStyle, addEvent, addClass, removeClass, hasClass, toggleClass, motion}
